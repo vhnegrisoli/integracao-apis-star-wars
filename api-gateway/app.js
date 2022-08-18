15268c6d1634ff1ed81c281921875f2666179d68
vhnegrisoli <victorhugonegrisoli.ccs@hotmail.com>
2022-08-18 16:51:53 -0300

    Adiciona Dockerfile e deploy no Kubernetes

--- api-gateway/app.js
+++ api-gateway/app.js
@@ -3,9 +3,11 @@ import httpProxy from "express-http-proxy";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const MOVIE_API_URL = process.env.MOVIE_API_URL || "http://localhost:8081";
+const CHARACTER_API_URL = process.env.CHARACTER_API_URL || "http://localhost:8082";
 
-const starWarsMovieApiService = httpProxy("http://localhost:8081");
-const starWarsCharacterApiService = httpProxy("http://localhost:8082");
+const starWarsMovieApiService = httpProxy(MOVIE_API_URL);
+const starWarsCharacterApiService = httpProxy(CHARACTER_API_URL);
 
 app.use("/api/movies", (req, res, next) => {
   return starWarsMovieApiService(req, res, next);
